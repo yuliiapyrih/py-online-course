3fa5c6e7cf00c0afbafa3873cccbcf06b7625695
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2024-09-29 02:46:04 +0300

    implement py-online-course

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
+from __future__ import annotations
+from math import ceil
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(day: int) -> int:
+        return ceil(day / 7)
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> OnlineCourse:
+        return cls(
+            name=course_dict["name"],
+            description=course_dict["description"],
+            weeks=cls.days_to_weeks(course_dict["days"])
+        )
